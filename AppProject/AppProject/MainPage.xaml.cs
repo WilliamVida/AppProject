d599c66885f7e21b3925864c0cad6232a25ad0e3
WILLIAM VIDA <G00356773@gmit.ie>
2018-11-03 13:08:57 +0000

    Add project files.

---
+++ AppProject/AppProject/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppProject
+{
+    public partial class MainPage : ContentPage
+    {
+        const double TAXABLE_CORPORATE_PROFITS = 848.57;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Earnings()
+        {
+
+        }
+
+
+    }
+}
