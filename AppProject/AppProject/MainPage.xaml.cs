fde119853b4019d502fee23e6edf840036885332
WILLIAM VIDA <G00356773@gmit.ie>
2018-11-05 11:33:15 +0000

    Sum button

--- AppProject/AppProject/MainPage.xaml.cs
+++ AppProject/AppProject/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace AppProject
 {
@@ -11,17 +12,22 @@ namespace AppProject
     {
         const double TAXABLE_CORPORATE_PROFITS = 848.57;
 
+        myAdder calc = new myAdder();
 
         public MainPage()
         {
             InitializeComponent();
+            this.BindingContext = calc;
         }
 
-        private void Earnings()
+        private void submit_Clicked(object sender, EventArgs e)
         {
+            double corprev = Convert.ToDouble(AllRev.Value);
+            double tax = corprev * TAXABLE_CORPORATE_PROFITS;
+            var taxOutput = String.Format("Total Revenue: ${0:0.00} Billion", tax);
 
+            Earnings1.Text = taxOutput;
         }
-
 
     }
 }
