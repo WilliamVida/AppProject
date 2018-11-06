7caff46ab414cca63f863e576109de67a9abf165
William <33327242+WilliamVida@users.noreply.github.com>
2018-11-06 12:41:32 +0000

    Update MainPage.xaml.cs

--- AppProject/AppProject/MainPage.xaml.cs
+++ AppProject/AppProject/MainPage.xaml.cs
@@ -10,15 +10,17 @@ namespace AppProject
 {
     public partial class MainPage : ContentPage
     {
+        const double LESS_THAN_25000 = 20;
+        const double SECOND_BRACKET = 0;
+        const double THIRD_BRACKET = 0;
+        const double FOURTH_BRACKET = 0;
+        const double FIFTH_BRACKET = 0;
 
         const double TAXABLE_CORPORATE_PROFITS = 848.57;
-
-        myAdder calc = new myAdder();
 
         public MainPage()
         {
             InitializeComponent();
-            this.BindingContext = calc;
         }
 
         private void submit_Clicked(object sender, EventArgs e)
@@ -26,11 +28,34 @@ namespace AppProject
             double corprev = Convert.ToDouble(CorporateTaxRate.Value);
             double tax = (corprev / 100) * TAXABLE_CORPORATE_PROFITS;
 
-
             var taxOutput = String.Format("Total Revenue: ${0:0.00} Billion", tax);
 
             TotalRevenue.Text = taxOutput;
         }
 
+        //private void CorporateTaxRate_ValueChanged(object sender, ValueChangedEventArgs e)
+        //{
+        //    var totRev = CorporateTaxRate.Value / 100 * TAXABLE_CORPORATE_PROFITS;
+        //    var taxOutput = String.Format("Total Revenue: ${0:0.00} Billion", totRev);
+
+        //    RevenueTest.Text = taxOutput;
+        //}
+
+        //private void LessThan25_ValueChanged(object sender, ValueChangedEventArgs e)
+        //{
+        //    var totRev = LessThan25.Value / 100 * LESS_THAN_25000;
+        //    var taxOutput = String.Format("Total Revenue: ${0:0.00} Billion", totRev);
+
+        //    RevenueTest.Text = taxOutput;
+        //}
+
+        private void TotalSliderRevenue_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var totRev = (LessThan25.Value / 100 * LESS_THAN_25000) + 
+                         (CorporateTaxRate.Value / 100 * TAXABLE_CORPORATE_PROFITS);
+
+            var taxOutput = String.Format("Total Revenue: ${0:0.00} Billion", totRev);
+            RevenueTest.Text = taxOutput;
+        }
     }
 }
