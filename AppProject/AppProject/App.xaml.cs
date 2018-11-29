235f260811cbeda79598ddf849cc596a01a933ea
William <33327242+WilliamVida@users.noreply.github.com>
2018-11-29 12:03:05 +0000

    Update App.xaml.cs

--- AppProject/AppProject/App.xaml.cs
+++ AppProject/AppProject/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +11,7 @@ namespace AppProject
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
