ae8c2f5c29bcc048ab163c9647afaf1dcd21a293
WILLIAM VIDA <G00356773@gmit.ie>
2018-12-16 01:42:26 +0000

    Revenue and other bits.

--- AppProject/AppProject/MainPage.xaml.cs
+++ AppProject/AppProject/MainPage.xaml.cs
@@ -10,15 +10,15 @@ namespace AppProject
 {
     public partial class MainPage : ContentPage
     {
-        const double FIRST_BRACKET = 1028.125; //1144545
-        const double SECOND_BRACKET = 959.3875; //8998075
-        const double THIRD_BRACKET = 1294.983333333333; //6997737
-        const double FOURTH_BRACKET = 762.6380952380952; //4803327
-        const double FIFTH_BRACKET = 1244.791666666667; //1563,650
-        const double SIXTH_BRACKET = 2102.581481481481; //2094906
-        const double TAXABLE_CORPORATE_PROFITS = 977.1428571428571;
-
-        double surdef;
+        const double FIRST_BRACKET = 1128.1861937619655392469687300574; // 1128.00
+        const double SECOND_BRACKET = 1050.4428496729419272495213784301; // 1050.00
+        const double THIRD_BRACKET = 1415.0590024994678649042756860242; // 1415.00
+        const double FOURTH_BRACKET = 902.68597286291666655520102105934; // 903.00
+        const double FIFTH_BRACKET = 1364.8656981227401759380982769623; // 1365.00
+        const double SIXTH_BRACKET = 2233.7009578222122864613911933631; //2234.00
+        const double PAYROLL = 7753.3333333333333333333333333333; // 7753.00
+        const double TAXABLE_CORPORATE_PROFITS = 848.57142857142857142857142857143; // 849.00
+        const double MISC_REVENUE = 269;
 
         public MainPage()
         {
@@ -38,8 +38,9 @@ namespace AppProject
                          (FourthBracket.Value / 100 * FOURTH_BRACKET) +
                          (FifthBracket.Value / 100 * FIFTH_BRACKET) +
                          (SixthBracket.Value / 100 * SIXTH_BRACKET) +
-                         (CorporateTaxRate.Value / 100 * TAXABLE_CORPORATE_PROFITS);
-            surdef = totRev;
+                         (PayrollRate.Value / 100 * PAYROLL) +
+                         (CorporateTaxRate.Value / 100 * TAXABLE_CORPORATE_PROFITS) +
+                         (MISC_REVENUE);
 
             var revenueFormat = String.Format("Total Revenue: ${0:0.00} Billion", totRev);
             TotalRevenue.Text = revenueFormat;
@@ -64,7 +65,9 @@ namespace AppProject
                          (FourthBracket.Value / 100 * FOURTH_BRACKET) +
                          (FifthBracket.Value / 100 * FIFTH_BRACKET) +
                          (SixthBracket.Value / 100 * SIXTH_BRACKET) +
-                         (CorporateTaxRate.Value / 100 * TAXABLE_CORPORATE_PROFITS);
+                         (PayrollRate.Value / 100 * PAYROLL) +
+                         (CorporateTaxRate.Value / 100 * TAXABLE_CORPORATE_PROFITS) +
+                         (MISC_REVENUE);
 
             double totSpend = HealthMan.Value + SocialSecurityMan.Value + AgricultureMan.Value + VeteransMan.Value + TransportationMan.Value
                              + MilitaryDisc.Value + VeteransDisc.Value + EducationDisc.Value + GovernmentDisc.Value + HousingDisc.Value
@@ -86,8 +89,6 @@ namespace AppProject
                 var spendingFormat = String.Format("Budget Balanced");
                 SurplusBalanceDeficit.Text = spendingFormat;
             }
-
-
         }
     }
 }
